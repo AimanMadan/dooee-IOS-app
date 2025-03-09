4cb7db48e5f879ddcceec60378694e4465ff836c
Aiman Madan <64807286+AimanMadan@users.noreply.github.com>
2025-03-09 00:33:47 -0800

    init

---
+++ To do List/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  To do List
+//
+//  Created by Aiman  on 3/8/25.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
